43f2ccfbda0983d9c281c6cd75f2c6cbffc0f3b2
Amita Sachan <sachan.amita33@gmail.com>
2017-01-21 17:20:31 +0530

    Added Authorize.net Code and Document

---
+++ Authorize.net/Allocat.CustomService.Authorize.Net/Allocat.CustomService.Authorize.Net/BusinessEntities/Address.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Allocat.CustomService.Authorize.Net.BusinessEntities
+{
+    [DataContract]
+   public class AddressInfo
+    {
+        [DataMember]
+        public string Address { get; set; }
+        [DataMember]
+        public string City { get; set; }
+        [DataMember]
+        public string ZipCode { get; set; }
+    }
+}
---
+++ Authorize.net/Allocat.CustomService.Authorize.Net/Allocat.CustomService.Authorize.Net/BusinessEntities/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Allocat.CustomService.Authorize.Net.BusinessEntities
+{
+    [DataContract]
+    public class BankAccount
+    {
+        [DataMember]
+        public string BankAccountNumber { get; set; }
+        [DataMember]
+        public string RoutingNumber { get; set; }
+        [DataMember]
+        public string AccountType { get; set; }
+        [DataMember]
+        public string ECheckType { get; set; }
+        [DataMember]
+        public string NameOnAccount { get; set; }
+        [DataMember]
+        public string BankName { get; set; }
+
+    }
+}
