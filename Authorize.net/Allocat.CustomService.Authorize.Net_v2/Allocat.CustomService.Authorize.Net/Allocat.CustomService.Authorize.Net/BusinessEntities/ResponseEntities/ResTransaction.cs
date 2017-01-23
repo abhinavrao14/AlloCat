f7c18afbe44c289bf3397144f21d0393f043a5cd
Amita Sachan <sachan.amita33@gmail.com>
2017-01-23 10:47:30 +0530

    Authorize.net v2 code without test application

---
+++ Authorize.net/Allocat.CustomService.Authorize.Net_v2/Allocat.CustomService.Authorize.Net/Allocat.CustomService.Authorize.Net/BusinessEntities/ResponseEntities/ResTransaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace Allocat.CustomService.Authorize.Net.BusinessEntities.ResponseEntities
+{
+    [DataContract]
+   public class ResTransaction
+    {
+        [DataMember]
+        public string TransactionId { get; set; }
+        [DataMember]
+        public string ResponseCode { get; set; }
+        [DataMember]
+        public string MessageDescription { get; set; }
+        [DataMember]
+        public string AuthCode { get; set; }
+        [DataMember]
+        public string MessageCode { get; set; }
+        [DataMember]
+        public string ErrorCode { get; set; }
+        [DataMember]
+        public string ErrorText { get; set; }
+    }
+}
